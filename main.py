d3975181d8011c591e650e95940e8143e3880f65
MJ <MJ@tsf-484-wpa-4-028.epfl.ch>
2017-05-09 14:20:16 +0200

    don't understand

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def write_birth_check():
     tail = [head] + tail
 
     s = '\n* '.join(tail)
+    print(s)
     write_on_page(s)
 
 
@@ -37,17 +38,21 @@ def pretty(list_facts):
     list_pretty = []
     for elem in list_facts:
         if ERROR_DATE in elem.name:
-            list_pretty.append(elem.name + " : [[" + elem.propositions[0].name + "]] né en " + elem.propositions[1].name + " et mort en " + elem.propositions[2].name)
+            list_pretty.append(elem.name + " : [[" + elem.propositions[0].name + "]] né en " + elem.propositions[1].name
+                + " et mort en " + elem.propositions[2].name)
         elif WARNING_ENCOUNTER in elem.name:
-            list_pretty.append(elem.name + " : [[" + elem.propositions[3].name + "]] et [[" + elem.propositions[4].name + "]] se sont rencontrés à [[" + elem.propositions[1].name + "]] et à [[" + elem.propositions[2].name + "]] en même temps à la date " + elem.propositions[0].name)
+            list_pretty.append(elem.name + " : [[" + elem.propositions[3].name + "]] et [[" + elem.propositions[4].name
+                + "]] se sont rencontrés à [[" + elem.propositions[1].name + "]] et à [[" + elem.propositions[2].name
+                + "]] en même temps à la date " + elem.propositions[0].name)
         elif ERROR_BIRTH in elem.name:
-            list_pretty.append(elem.name + " : [[" + elem.propositions[0].name + "]] né en " + elem.propositions[
-                1].name + " et né en " + elem.propositions[2].name)
+            list_pretty.append(elem.name + " : [[" + elem.propositions[0].name + "]] né en " + elem.propositions[1].name
+                + " et né en " + elem.propositions[2].name)
         elif ERROR_DEATH in elem.name:
-            list_pretty.append(elem.name + " : [[" + elem.propositions[0].name + "]] mort en " + elem.propositions[
-                1].name + " et mort en " + elem.propositions[2].name)
+            list_pretty.append(elem.name + " : [[" + elem.propositions[0].name + "]] mort en "
+                + elem.propositions[1].name + " et mort en " + elem.propositions[2].name)
         elif ERROR_ELECTION in elem.name:
-            list_pretty.append(elem.name + " : [[" + elem.propositions[6].name + "]] (" + elem.propositions[0].name + " / " + elem.propositions[1].name + ")"+ " est élu en " + elem.propositions[2].name + " à [[" + elem.propositions[5].name + "]]")
+            list_pretty.append(elem.name + " : [[" + elem.propositions[6].name + "]] (" + elem.propositions[0].name
+                + " / " + elem.propositions[1].name + ")"+ " est élu en " + elem.propositions[2].name + " à [[" + elem.propositions[5].name + "]]")
     return list_pretty
 
 if __name__ == '__main__':
